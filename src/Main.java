7bc02bd1c3025a62b5f31c3e25bc92909184a7fd
Anna Markova <anbeley@mail.ru>
2023-05-11 14:11:51 +0300

    1st task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int amount = 13_676;
+        int mile = 20;
+
+        int bonus = amount / mile;
+        System.out.println("Стоимость билета: " + amount);
+        System.out.println("Количество бонусов: " + bonus);
+
     }
 }
